b8c8331f3be65c4ef3f171911818732daf221fb1
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2022-08-09 21:28:42 -0400

    fix: bad diff split

--- src/translator/expand.js
+++ src/translator/expand.js
@@ -9,13 +9,13 @@ async function expandInserts(string){
 	let output = string;
 
 	// Block aliases
-	output = output.replaceAll(REGEX.aliasBlock, (_match, p1, p2) => `${p1} \\footnote{${doublePar(p2, "raw", handlers)}}`);
+	output = output.replaceAll(REGEX.aliasBlock, (_match, p1, p2) => `${p1} \\footnote{${doublePar(p2, "raw")}}`);
 	// Embeds : blocks
-	output = output.replaceAll(REGEX.embedBlock, (_match, _p1, _p2, p3) => blockEmbed(p3, "latex", handlers));
+	output = output.replaceAll(REGEX.embedBlock, (_match, _p1, _p2, p3) => blockEmbed(p3, "latex"));
 	// Embeds : pages
-	output = output.replaceAll(REGEX.embedPage, (_match, _p1, _p2, p3) => pageEmbed(p3, "latex", handlers));
+	output = output.replaceAll(REGEX.embedPage, (_match, _p1, _p2, p3) => pageEmbed(p3, "latex"));
 	// `(())` markup
-	output = output.replaceAll(REGEX.doublePar, (_match, p1) => doublePar(p1, "latex", handlers));
+	output = output.replaceAll(REGEX.doublePar, (_match, p1) => doublePar(p1, "latex"));
 
 	return output;
 }
