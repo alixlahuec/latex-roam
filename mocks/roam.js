56089a9b2f6e8bf4c88181bf5bd9cc6608cadb1a
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2022-08-10 21:01:59 -0400

    feat: improve Roam mock

--- mocks/roam.js
+++ mocks/roam.js
@@ -1,11 +1,29 @@
 // TODO: improve mock return values
-export function queryBlockContents(_uid){
-	return {
-		string: "Block text",
-		children: [],
+
+export const defaultUID = "some_uid";
+export const uidWithFormatting = "with_formatting";
+
+export const sampleBlocks = {
+	[uidWithFormatting]: {
+		string: "Block with **bold text**, some ^^highlighting^^ ((see other work))",
 		order: 0,
-		heading: "none",
+		heading: 0,
+		"view-type": "document",
+		"text-align": "left"
+	},
+	[defaultUID]: {
+		string: "Text extracted from block reference",
+		order: 0,
+		heading: 0,
 		"view-type": "bulleted",
 		"text-align": "left"
-	};
+	}
+};
+
+export function getBlockText(uid){
+	return Object.keys(sampleBlocks).includes(uid) ? [[sampleBlocks[uid].string]] : [];
+}
+
+export function queryBlockContents(uid){
+	return sampleBlocks[uid] || sampleBlocks[defaultUID];
 }
--- tests/translator/doublePar.test.js
+++ tests/translator/doublePar.test.js
@@ -2,23 +2,17 @@ import REGEX from "../../src/translator/regex";
 import { asyncReplaceAll } from "../../src/utils";
 import doublePar from "../../src/translator/doublePar";
 
+import { defaultUID, sampleBlocks } from "../../mocks/roam";
 
-jest.mock("Roam", () => ({
-	getBlockText: (input) => {
-		return input == "some_uid"
-			? [["Text extracted from block reference"]]
-			: [];
-	}
-}));
 
 describe("Parsing content inside of double parentheses", () => {
 	test("Block reference - raw, simple text", async() => {
-		expect(await doublePar("some_uid", "raw"))
-			.toBe("Text extracted from block reference");
+		expect(await doublePar(defaultUID, "raw"))
+			.toBe(sampleBlocks[defaultUID].string);
 	});
 	test("Block reference - LaTeX, simple text", async() => {
-		expect(await doublePar("some_uid", "latex"))
-			.toBe("Text extracted from block reference");
+		expect(await doublePar(defaultUID, "latex"))
+			.toBe(sampleBlocks[defaultUID].string);
 	});
 	test("Simple text - raw", async() => {
 		expect(await doublePar("Parenthesised text", "raw"))
@@ -36,16 +30,16 @@ test("Replace all - Double parentheses", async() => {
 	expect(await asyncReplaceAll(input, REGEX.doublePar, async(_match, p1) => await doublePar(p1, "latex")))
 		.toBe("This is reminiscent of X's model \\footnote{see Their Work} and Y's work \\footnote{see their Model} also");
     
-	const withBlockRef = "Refer to ((some_uid))";
+	const withBlockRef = `Refer to ((${defaultUID}))`;
     
 	expect(await asyncReplaceAll(withBlockRef, REGEX.doublePar, async(_match, p1) => await doublePar(p1, "latex")))
-		.toBe("Refer to Text extracted from block reference");
+		.toBe("Refer to " + sampleBlocks[defaultUID].string);
 });
 
 test("Replace all - Block aliases", async() => {
-	const input = "This is reminiscent of [X's model](((some_uid))) of the same problem";
+	const input = `This is reminiscent of [X's model](((${defaultUID}))) of the same problem`;
 
 	// Debugging because the real code causes an infinite loop
 	expect(await asyncReplaceAll(input, REGEX.aliasBlock, async(_match, p1, p2) => `${p1} \\footnote{${await doublePar(p2, "raw")}}`))
-		.toBe("This is reminiscent of X's model \\footnote{Text extracted from block reference} of the same problem");
+		.toBe(`This is reminiscent of X's model \\footnote{${sampleBlocks[defaultUID].string}} of the same problem`);
 });
