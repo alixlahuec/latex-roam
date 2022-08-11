1f385387ffabfb5193931503989eecdc8784fcf7
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2022-08-10 21:20:01 -0400

    feat: refactor tests for doublePar

--- tests/translator/doublePar.test.js
+++ tests/translator/doublePar.test.js
@@ -22,22 +22,43 @@ describe("Parsing content inside of double parentheses", () => {
 	);
 });
 
-test("Replace all - Double parentheses", async() => {
-	const input = "This is reminiscent of X's model ((see Their Work)) and Y's work ((see their Model)) also";
+describe("Replace all - Double parentheses", () => {
+	const cases = [
+		[
+			"Regular text",
+			"This is reminiscent of X's model ((see Their Work)) and Y's work ((see their Model)) also",
+			"This is reminiscent of X's model \\footnote{see Their Work} and Y's work \\footnote{see their Model} also"
+		],
+		[
+			"Block UID",
+			`Refer to ((${defaultUID}))`,
+			"Refer to " + sampleBlocks[defaultUID].string
+		]
+	];
 
-	expect(await asyncReplaceAll(input, REGEX.doublePar, async(_match, p1) => await doublePar(p1, "latex")))
-		.toBe("This is reminiscent of X's model \\footnote{see Their Work} and Y's work \\footnote{see their Model} also");
-    
-	const withBlockRef = `Refer to ((${defaultUID}))`;
-    
-	expect(await asyncReplaceAll(withBlockRef, REGEX.doublePar, async(_match, p1) => await doublePar(p1, "latex")))
-		.toBe("Refer to " + sampleBlocks[defaultUID].string);
+	test.each(cases)(
+		"%# - %s",
+		async(_id, input, expectation) => {
+			expect(await asyncReplaceAll(input, REGEX.doublePar, async(_match, p1) => await doublePar(p1, "latex")))
+				.toBe(expectation);
+		}
+	);
 });
 
-test("Replace all - Block aliases", async() => {
-	const input = `This is reminiscent of [X's model](((${defaultUID}))) of the same problem`;
+describe("Replace all - Block aliases", () => {
+	const cases = [
+		[
+			"Block with simple text",
+			`This is reminiscent of [X's model](((${defaultUID}))) of the same problem`,
+			`This is reminiscent of X's model \\footnote{${sampleBlocks[defaultUID].string}} of the same problem`
+		]
+	];
 
-	// Debugging because the real code causes an infinite loop
-	expect(await asyncReplaceAll(input, REGEX.aliasBlock, async(_match, p1, p2) => `${p1} \\footnote{${await doublePar(p2, "raw")}}`))
-		.toBe(`This is reminiscent of X's model \\footnote{${sampleBlocks[defaultUID].string}} of the same problem`);
+	test.each(cases)(
+		"%# - %s",
+		async(_id, input, expectation) => {
+			expect(await asyncReplaceAll(input, REGEX.aliasBlock, async(_match, p1, p2) => `${p1} \\footnote{${await doublePar(p2, "raw")}}`))
+				.toBe(expectation);
+		}
+	);
 });
