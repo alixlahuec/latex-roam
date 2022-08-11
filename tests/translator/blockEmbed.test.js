7a8f294c04d5fcd333e92dc0ae7ed4c67940720e
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2022-08-10 21:02:42 -0400

    feat: add tests for blockEmbed

---
+++ tests/translator/blockEmbed.test.js
@@ -0,0 +1,22 @@
+import blockEmbed from "../../src/translator/blockEmbed";
+
+import { defaultUID, sampleBlocks, uidWithFormatting } from "../../mocks/roam";
+import { grabRawText, raw } from "../../src/translator/common";
+
+
+describe("Parsing content of block embeds", () => {
+	const cases = [
+		[defaultUID, "raw", () => sampleBlocks[defaultUID].string],
+		[defaultUID, "latex", () => sampleBlocks[defaultUID].string],
+		[uidWithFormatting, "raw", async() => await grabRawText(sampleBlocks[uidWithFormatting])],
+		[uidWithFormatting, "latex", async() => await raw(sampleBlocks[uidWithFormatting])],
+	];
+
+	test.each(cases)(
+		"%# - %s (mode: %s)",
+		async(uid, mode, expectation) => {
+			expect(await blockEmbed(uid, mode))
+				.toBe(await expectation());
+		}
+	);
+});
