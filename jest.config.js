4c2d6b11f13df4f5107c951fdb3ca4cc77cae4b0
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2022-08-12 23:49:05 -0400

    infra: transform client-zip for jest
    client-zip has a strange export, which prevents writing Jest tests with it unless it's explicitly targeted for transform

--- jest.config.js
+++ jest.config.js
@@ -179,15 +179,16 @@ module.exports = {
   
     // A map from regular expressions to paths to transformers
     transform: {
+      "node_modules/(client-zip)/.+\\.(j|t)sx?$": "babel-jest",
       "\\.jsx?$": "babel-jest",
       "^.+\\.css$": "jest-transform-css"
     },
   
     // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-    // transformIgnorePatterns: [
-    //   "\\\\node_modules\\\\",
-    //   "\\.pnp\\.[^\\\\]+$"
-    // ],
+    transformIgnorePatterns: [
+      "\\\\node_modules\\\\(?!(somePkg)|client-zip)",
+      "\\.pnp\\.[^\\\\]+$"
+    ],
   
     // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
     // unmockedModulePathPatterns: undefined,
