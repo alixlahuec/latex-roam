7ef4ecb0f93385fef90fd2e2ea8d26cd9e2e96ae
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2022-08-13 21:53:35 -0400

    chore: update dependencies (#8)

--- src/components/ExportButton/index.jsx
+++ src/components/ExportButton/index.jsx
@@ -12,7 +12,7 @@ function ExportButton({ uid }){
 	const [isDialogOpen, { on: openDialog, off: closeDialog }] = useBool(false);
 
 	return <>
-		<Button className={CustomClasses.BUTTON_CLASS} icon="rect-width" minimal={true} onClick={openDialog} text="LaTeX" />
+		<Button className={CustomClasses.BUTTON_CLASS} icon="code" minimal={true} onClick={openDialog} text="LaTeX" />
 		<ExportDialog isOpen={isDialogOpen} onClose={closeDialog} uid={uid} />
 	</>;
 }
