509ed31fec128a2fd92cfa2896dce88755135b82
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2022-08-14 20:32:55 -0400

    chore: remove outdated "showName" prop

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -34,10 +34,8 @@ export const globalTypes = {
             icon: 'circlehollow',
             // Array of plain string values or MenuItem shape (see below)
             items: ['light', 'dark'],
-            // Property that specifies if the name of the item will be displayed
-            showName: true,
             // Change title based on selected value
-            dynamicTitle: true,
+            dynamicTitle: true
         },
     },
 };
