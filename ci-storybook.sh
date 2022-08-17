850e237ecc5083ffb97a1263c0fe29c0945dde01
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2022-08-16 21:39:57 -0400

    move Storybook testing to script, call in release workflows

---
+++ ci-storybook.sh
@@ -0,0 +1,13 @@
+# Storybook test runner (https://storybook.js.org/docs/react/writing-tests/test-runner#set-up-ci-to-run-tests)
+
+# Install Playwright
+sudo apt-get update
+npx playwright install --with-deps
+
+# Build Storybook
+npm run build-storybook
+
+# Serve Storybook and run tests
+npx concurrently -k -s first -n "SB,TEST" -c "magenta,blue" \
+    npx http-server storybook-static --port 6006 --silent \
+    npx wait-on tcp:6006 && npm run ci:test-storybook
